e340036aa12c85415ecf9f7abc85c5f44d4dfba3
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-06-10 10:03:21 +0900

    Modify

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 set :application, "howl"
-set :repo_url, "git@example.com:yusoshi/howl.git"
+set :repo_url, "git@github.com:yusoshi/howl.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
