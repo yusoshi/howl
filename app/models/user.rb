e3e9f9b0b13e10ba45e39f3f0949e09c72a34df2
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-07-19 13:52:05 +0900

    Create method for find or create  a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         authentication_keys: [:name], :omniauthable
+         :omniauthable,
+         authentication_keys: [:name]
 
   # ユーザーセッションのバリデーション
   validates :name, presence: true, uniqueness: true
@@ -26,5 +27,19 @@ class User < ApplicationRecord
 
   def email_changed?
     false
+  end
+
+  # SNS認証
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:        auth.uid,
+        provider:   auth.provider,
+        password:   Devise.friendly_token[0.20]
+      )
+    end
+    user
   end
 end
