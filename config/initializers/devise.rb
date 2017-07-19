d69f78916bf48479b76f2115573b6574112e9eba
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-07-19 15:00:00 +0900

    Change CONSTANT

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -274,5 +275,4 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :twitter, ENV["API Key"], ENV["API Secret"]
 end
