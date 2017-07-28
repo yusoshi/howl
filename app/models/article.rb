26c1a6c81e944bc2cbb080bc3a7255efafa3f994
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-07-29 00:05:00 +0900

    Modify models and views

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,8 @@ class Article < ApplicationRecord
   #アソシエーション
   belongs_to :user
 
-  has_many :article_tags
-  has_many :tags, through: :article_tags
+  has_many :articles_tags
+  has_many :tags, through: :articles_tags
 
   #バリデーション
   validates :title, presence: true
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
   #アソシエーション
-  has_many  :article_tags
-  has_many  :articles, through: :article_tags
+  has_many  :articles_tags
+  has_many  :articles, through: :articles_tags
 
   #バリデーション
   validates :name, presence: true, uniqueness: true
