0914478f89021ae84958654dc406aab00766b3cf
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-07-29 11:36:03 +0900

    Set root to articles/index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
   root 'articles#index'
+  resources :articles, only: [:index]
 end
