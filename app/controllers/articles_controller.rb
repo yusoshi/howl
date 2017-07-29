74047dd5fb31038a916cffc85e231d2553690b76
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-07-29 18:34:27 +0900

    Create link to selected article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all.includes(:user)
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   }
 
   root 'articles#index'
-  resources :articles, only: [:index]
+  resources :articles, only: [:index, :show]
 end
