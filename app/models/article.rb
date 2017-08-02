84db7454d6de3798894c731e0a942b6be6c126d2
yusoshi <nsymgnk0405@yahoo.co.jp>
2017-08-02 13:06:41 +0900

    create view

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,4 +11,10 @@ class Article < ApplicationRecord
   #バリデーション
   validates :title, presence: true
   validates :body, presence: true
+
+  # 最終更新日のフォーマットを整える
+  def updated_at_format
+    raw_updated_at = self.updated_at
+    return raw_updated_at
+  end
 end
